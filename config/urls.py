ac817ceebc6bd376738f01435e9706977cf8050d
dotory-60 <busdotory@gmail.com>
2026-01-08 03:11:15 +0900

    ✨ feat(api): add schema and swagger ui routes

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
 from django.urls import path
-
 from drf_spectacular.views import (
-    SpectacularAPIView,     # 프로젝트의 API 설계도(Schema)를 생성합니다.
-    SpectacularRedocView,   # 정적이고 깔끔한 문서 위주의 화면입니다.
-    SpectacularSwaggerView, # 브라우저에서 직접 API를 테스트(Try it out)할 수 있는 화면입니다.
+    SpectacularAPIView,  # 프로젝트의 API 설계도(Schema)를 생성합니다.
+    SpectacularRedocView,  # 정적이고 깔끔한 문서 위주의 화면입니다.
+    SpectacularSwaggerView,  # 브라우저에서 직접 API를 테스트(Try it out)할 수 있는 화면입니다.
 )
 
 # 문서화 관련 URL
 docs_urlpatterns = [
     # Schema URL
-    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
-
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Swagger URL
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name="swagger-ui"),
-
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     # Redoc URL
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-] + docs_urlpatterns # 문서화 관련 URL 합치기
+    path("admin/", admin.site.urls),
+] + docs_urlpatterns  # 문서화 관련 URL 합치기
